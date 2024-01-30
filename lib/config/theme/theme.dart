232e6c66176693bdf50d9f16c5b728ffe67c5853
GulshirinAved <suleymanovaalika@gmail.com>
2024-01-30 11:19:44 +0500

    banner is added

--- lib/config/theme/theme.dart
+++ lib/config/theme/theme.dart
@@ -5,6 +5,7 @@ class AppColors {
   static Color orangeColor = Color(0xffF7A78C);
   static Color darkOrangeColor = Color(0xffFD5E29);
   static Color greyColor = Color(0xff8D98A9);
+  static Color lightGreyColor = Color(0xffE1E1E1);
 }
 
 class AppFonts {
--- lib/presentation/Screens/home/components/banner.dart
+++ lib/presentation/Screens/home/components/banner.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:isleg_ecommerce/blocs/home/banner_indicator/banner_indicator_bloc.dart';
 import 'package:isleg_ecommerce/config/constants/constants.dart';
+import 'package:isleg_ecommerce/config/theme/theme.dart';
 
 class ImageSlider extends StatefulWidget {
   const ImageSlider({super.key});
@@ -44,68 +45,77 @@ class _ImageSliderState extends State<ImageSlider> {
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => BannerIndicatorBloc(),
-      child: Stack(
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height / 4,
-            width: double.infinity,
-            child: BlocBuilder<BannerIndicatorBloc, BannerIndicatorState>(
-              builder: (context, state) {
-                return PageView.builder(
-                  controller: _pageController,
-                  onPageChanged: (value) {
-                    context
-                        .read<BannerIndicatorBloc>()
-                        .add(PressEvent(eventIndex: value));
-                  },
-                  itemCount: sliderImages.length,
-                  itemBuilder: (context, index) {
-                    return Image.asset(
-                      sliderImages[index],
-                      fit: BoxFit.cover,
-                    );
-                  },
-                );
-              },
-            ),
-          ),
-          Positioned(
-            bottom: 10.h,
-            left: 0,
-            right: 0,
-            child: SizedBox(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: List<Widget>.generate(
-                    sliderImages.length,
-                    (index) =>
-                        BlocBuilder<BannerIndicatorBloc, BannerIndicatorState>(
-                          builder: (context, state) {
-                            return GestureDetector(
-                              onTap: () {
-                                _pageController.animateToPage(index,
-                                    duration: Duration(milliseconds: 500),
-                                    curve: Curves.easeIn);
-                                context
-                                    .read<BannerIndicatorBloc>()
-                                    .add(PressEvent(eventIndex: index));
-                              },
-                              child: Container(
-                                margin: EdgeInsets.only(right: 5.w),
-                                child: CircleAvatar(
-                                  radius: 4,
-                                  backgroundColor: state.stateIndex == index
-                                      ? Colors.amber
-                                      : Colors.black,
-                                ),
-                              ),
-                            );
-                          },
-                        )),
+      child: Padding(
+        padding: EdgeInsets.symmetric(vertical: 8.h),
+        child: Stack(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 4,
+              width: double.infinity,
+              child: BlocBuilder<BannerIndicatorBloc, BannerIndicatorState>(
+                builder: (context, state) {
+                  return PageView.builder(
+                    controller: _pageController,
+                    onPageChanged: (value) {
+                      context
+                          .read<BannerIndicatorBloc>()
+                          .add(PressEvent(eventIndex: value));
+                    },
+                    itemCount: sliderImages.length,
+                    itemBuilder: (context, index) {
+                      return Image.asset(
+                        sliderImages[index],
+                        fit: BoxFit.cover,
+                      );
+                    },
+                  );
+                },
               ),
             ),
-          ),
-        ],
+            Positioned(
+              bottom: 10.h,
+              left: 0,
+              right: 0,
+              child: SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: List<Widget>.generate(
+                      sliderImages.length,
+                      (index) => BlocBuilder<BannerIndicatorBloc,
+                              BannerIndicatorState>(
+                            builder: (context, state) {
+                              return GestureDetector(
+                                onTap: () {
+                                  _pageController.animateToPage(index,
+                                      duration: Duration(milliseconds: 500),
+                                      curve: Curves.easeIn);
+                                  context
+                                      .read<BannerIndicatorBloc>()
+                                      .add(PressEvent(eventIndex: index));
+                                },
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      shape: BoxShape.circle,
+                                      border: Border.all(
+                                          color: AppColors.whiteColor,
+                                          width: 1.5,
+                                          style: BorderStyle.solid)),
+                                  margin: EdgeInsets.only(right: 5.w),
+                                  child: CircleAvatar(
+                                    radius: 4,
+                                    backgroundColor: state.stateIndex == index
+                                        ? AppColors.darkOrangeColor
+                                        : AppColors.lightGreyColor,
+                                  ),
+                                ),
+                              );
+                            },
+                          )),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
